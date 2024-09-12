2a2b25a779e2979dbf0979e15b59ac58f158601b
Kcram <kevin.frrzr@eduge.ch>
2024-09-12 08:55:13 +0200

    feat: :construction: add home link, accessible but not finished
    The home link (/) is now accessible, but the full implementation is still in progress.

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,7 @@
 global $app;
 require '../vendor/autoload.php';
 
-use Mbnot\SteenApi\controllers\indexController;
+use mbnot\steenWeb\controllers\indexController;
+
+// page Home
+$app->get('/', [indexController::class, 'welcome']);
