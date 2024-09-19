14dec8f4ea91c01b7b17ff7c95940c9d9b8ee812
Kcram <kevin.frrzr@eduge.ch>
2024-09-19 09:18:52 +0200

    feat: :sparkles: add brand logo and integrate it into the layout
    The brand logo has been added and included in the layout, ensuring consistent display across all pages.

--- views/layout.php
+++ views/layout.php
@@ -13,7 +13,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container">
-                <a class="navbar-brand" href="#">Steen</a>
+                <a class="navbar-brand" href="#"><img src="/images/logomark.png" alt="logo of mark" width=70px></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
