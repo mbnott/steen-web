1a74d618d24cb553e591fda002adc76b2155edb6
Kcram <kevin.frrzr@eduge.ch>
2024-10-03 11:14:55 +0200

    refactor: Update namespace, page titles, and data submission
    Changed the namespace, updated page titles, and modified the data submission process for better consistency and functionality.

--- src/controllers/baseController.php
+++ src/controllers/baseController.php
@@ -2,7 +2,7 @@
 
 /**
  * Projet : steen-web
- * Auteur : Kevin FRRZR && Eden DNCSR
+ * Auteur : Kcram && Mbnot
  * Date : 29.08.2024
  * Description : This BaseController file serves as a foundation for all controllers,
  * containing variables and methods that are commonly needed.
@@ -19,7 +19,7 @@ class baseController
 
     public function __construct()
     {
-        $this->views = new PhpRenderer(__DIR__ . '/../../views', ['title' => 'Spectacle']);
+        $this->views = new PhpRenderer(__DIR__ . '/../../views', ['title' => 'steen-web']);
         $this->views->setLayout("layout.php");
     }
 }
