f429837225ee0226f5d9af5300e4335491b3eaa0
Kcram <kevin.frrzr@eduge.ch>
2024-10-17 09:15:25 +0200

    fix: rename Name Class apiManager to ApiManager

--- src/models/ApiManager.php
+++ src/models/ApiManager.php
@@ -2,7 +2,7 @@
 
 /**
  * Projet : steen-web
- * Auteur : Kevin FRRZR && Eden DNCSR
+ * Auteur : Kcram && Mbnot
  * Date : 29.08.2024
  * Description : This file handles the requests between the website and the API,
  * retrieving the necessary data required for the site's functionality.
@@ -15,7 +15,7 @@ namespace Mbnot\SteenWeb\models;
 
 use PDOException;
 
-class apiManager
+class ApiManager
 {
     
 }
