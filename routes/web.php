24c04e347af69db7e76bb5c4e1e34502028e9380
Kcram <kevin.frrzr@eduge.ch>
2024-10-17 09:14:28 +0200

    feat: add a new route for the search page
    - Implemented a new route in the routing system to enable direct access to the search page.

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,13 @@ require '../vendor/autoload.php';
 
 use Mbnot\SteenWeb\controllers\indexController;
 use Mbnot\SteenWeb\controllers\loginController;
+use Mbnot\SteenWeb\controllers\searchController;
 
 // page Home
 $app->get('/', [indexController::class, 'welcome']);
 
 // page Login
 $app->get('/login', [loginController::class, 'show']);
+
+// page Search
+$app->get('/search', [searchController::class, 'show']);
