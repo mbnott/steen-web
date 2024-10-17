05b22f5c717a8ece26dfdaadf2f07e994e91dbe5
Kcram <kevin.frrzr@eduge.ch>
2024-10-17 09:18:12 +0200

    fix: Updated page links and fixed outdated CSS link
    - Modified the links to various pages to ensure correct navigation and accessibility.

--- views/layout.php
+++ views/layout.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Steen-Web | Accueil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="./css/home.css"> 
+    <link rel="stylesheet" href="./css/style.css"> 
 </head>
 
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container">
-                <a class="navbar-brand" href="#"><img src="/images/logomark.png" alt="logo of mark" width=70px></a>
+                <a class="navbar-brand" href="/"><img src="/images/logomark.png" alt="logo of mark" width=70px></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -21,13 +21,13 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Games</a>
+                            <a class="nav-link" href="/games">Games</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Software</a>
+                            <a class="nav-link" href="/software">Software</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Login</a>
+                            <a class="nav-link" href="/login">Login</a>
                         </li>
                     </ul>
                 </div>
